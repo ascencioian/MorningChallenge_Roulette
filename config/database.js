fe524a7c929a4f0a73d823eca690bf4bab26bd61
Ian Ascencio <ian.ascencio.mancia@gmail.com>
2021-10-25 11:07:01 -0400

    global access

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://admin:HT4bS37vGpS8LyL@cluster1.yi7wc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'dbName': 'roulette'
+};
+
